8b40bb8312d7312088dcd415080a23273d37fb53
Igor Krysov <igorkrysov@mail.ru>
2019-12-09 18:45:15 +0600

    init

---
+++ src/ChatProvicer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers\..\..\packages\chat\src;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChatProvicer extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
